a46d854b93efb24b5fa6a5190e70930eae740ac4
0xali <137429355+sealali@users.noreply.github.com>
2024-10-07 15:32:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let input = document.getElementById("task");
-let btn = document.getElementById("btn");
-let out = document.getElementById("out");
-
-let data = [];
-let newDiv;
-
-
-btn.addEventListener("click",()=>{
-    data.push(input.value);
-    newDiv = document.createElement("li");
-newDiv.textContent = input.value;
-out.appendChild(newDiv);
-    
-})
+const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/24.98%2C%2067.28?unitGroup=metric&key=2EA5D5K697FT2HG9ZQXNC427G&contentType=json';
+var data;
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
